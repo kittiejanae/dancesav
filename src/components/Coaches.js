bb2bb06999961bc43c87c30df00b3587b9183032
kittiejanae <talkjay31@gmail.com>
2025-06-27 19:36:04 -0400

    coaches12

--- src/components/Coaches.js
+++ src/components/Coaches.js
@@ -103,68 +103,68 @@ const thecoaches = [
     }`,
     occupation: "Dance Abilities Coach",
   },
-  {
-    name: "Micah Lynch",
-    personPhoto: `${
-      require("src/images/Micah Lynch- Bee Company Coach - Ballet Instructor.jpg")
-        .default
-    }`,
-    occupation: "Ballet Instructor",
-  },
-  {
-    name: "Tatianna Jordan",
-    personPhoto: `${
-      require("src/images/Tatianna Jordan - Bee Company Coach - Ballet Instructor.jpg")
-        .default
-    }`,
-    occupation: "Ballet Instructor",
-  },
-  {
-    name: "Bekah Kerr",
-    personPhoto: `${
-      require("src/images/Bekah Kerr - Prep Team Coach - Ballet Instructor.jpg")
-        .default
-    }`,
-    occupation: "Prep Team Coach & Ballet Instructor",
-  },
-  {
-    name: "Shaquiese Little",
-    personPhoto: `${
-      require("src/images/Shaquiese Little - Prep Team Coach Hip Hop & Tumbling Instructor.jpg")
-        .default
-    }`,
-    occupation: "Prep Team Coach Hip Hop & Tumbling Instructor",
-  },
-  {
-    name: "Armelia Hartle",
-    personPhoto: `${
-      require("src/images/Armelia Hartle- Ballet Instructor.jpg").default
-    }`,
-    occupation: "Ballet Instructor",
-  },
-  {
-    name: "Rebecca Derst",
-    personPhoto: `${
-      require("src/images/Rebecca Derst  - Ballet Instructor.jpg").default
-    }`,
-    occupation: "Ballet Instructor",
-  },
-  {
-    name: "Mercedes Green",
-    personPhoto: `${
-      require("src/images/Mercedes Green - Hip Hop & Tumbling Instructor.jpg")
-        .default
-    }`,
-    occupation: "Hip Hop & Tumbling Instructor",
-  },
-  {
-    name: "Darrell Jordan",
-    personPhoto: `${
-      require("src/images/Darrell Jordan - Studio Operations Director.jpg")
-        .default
-    }`,
-    occupation: "Studio Operations Directors",
-  },
+  // {
+  //   name: "Micah Lynch",
+  //   personPhoto: `${
+  //     require("../images/Micah Lynch- Bee Company Coach - Ballet Instructor.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Ballet Instructor",
+  // },
+  // {
+  //   name: "Tatianna Jordan",
+  //   personPhoto: `${
+  //     require("src/images/Tatianna Jordan - Bee Company Coach - Ballet Instructor.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Ballet Instructor",
+  // },
+  // {
+  //   name: "Bekah Kerr",
+  //   personPhoto: `${
+  //     require("src/images/Bekah Kerr - Prep Team Coach - Ballet Instructor.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Prep Team Coach & Ballet Instructor",
+  // },
+  // {
+  //   name: "Shaquiese Little",
+  //   personPhoto: `${
+  //     require("src/images/Shaquiese Little - Prep Team Coach Hip Hop & Tumbling Instructor.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Prep Team Coach Hip Hop & Tumbling Instructor",
+  // },
+  // {
+  //   name: "Armelia Hartle",
+  //   personPhoto: `${
+  //     require("src/images/Armelia Hartle- Ballet Instructor.jpg").default
+  //   }`,
+  //   occupation: "Ballet Instructor",
+  // },
+  // {
+  //   name: "Rebecca Derst",
+  //   personPhoto: `${
+  //     require("src/images/Rebecca Derst  - Ballet Instructor.jpg").default
+  //   }`,
+  //   occupation: "Ballet Instructor",
+  // },
+  // {
+  //   name: "Mercedes Green",
+  //   personPhoto: `${
+  //     require("src/images/Mercedes Green - Hip Hop & Tumbling Instructor.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Hip Hop & Tumbling Instructor",
+  // },
+  // {
+  //   name: "Darrell Jordan",
+  //   personPhoto: `${
+  //     require("src/images/Darrell Jordan - Studio Operations Director.jpg")
+  //       .default
+  //   }`,
+  //   occupation: "Studio Operations Directors",
+  // },
 ];
 
 const CoachList = () => {
